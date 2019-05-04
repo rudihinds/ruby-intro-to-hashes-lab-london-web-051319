1692642084327274f1bf02ab78d478a0023ec559
rudi hinds <rudihinds@gmail.com>
2019-05-04 10:47:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,6 +12,8 @@ def monopoly
 end
 
 def monopoly_with_second_tier
+  monopoly = {}
+  
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
